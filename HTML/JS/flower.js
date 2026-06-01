719dc11df5e2292e0b427480ec8d9d716c4dde77
kyy-coding <zhakyap2@gmail.com>
2026-06-01 20:52:36 +0800

    Update flower.js

--- HTML/JS/flower.js
+++ HTML/JS/flower.js
@@ -1,1 +1,13 @@
-
+function showFlowers(){
+    const story =
+    document.getElementById("story-container");
+    const flowerScene =
+    document.getElementById("flower-scene");
+    if(story){
+        story.style.display = "none";
+    }
+    if(flowerScene){
+        flowerScene.classList.remove("hidden");
+    }
+    document.body.classList.remove("not-loaded");
+}
