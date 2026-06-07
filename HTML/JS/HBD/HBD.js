61d1b48c55d3f8a10a13787f90bccd3d121bd2e7
kyy-coding <zhakyap2@gmail.com>
2026-06-07 22:23:08 +0800

    Update HBD.js

--- HTML/JS/HBD/HBD.js
+++ HTML/JS/HBD/HBD.js
@@ -9,7 +9,7 @@ music:"https://kyy-coding.github.io/ProjectHTMl/Music/Mantra-Hujan-Kobo-Kanaeru.
 pages:[
 {
 background:"https://kyy-coding.github.io/ProjectHTMl/background/Kobo/Background1.jpeg",
-sticker:"https://kyy-coding.github.io/ProjectHTMl/Sticker/Kobo/Sticker1.gif",
+sticker:"https://tenor.com/id/view/kobo-kanaeru-こぼ-ホロライブ-hololive-kobo-gif-25201998",
 title:"HEEH KAMU",
 text:"Ini html pertamaku. Tolong tekan tombol lanjutnya pelan-pelan yaaa",
 typewriter: false
